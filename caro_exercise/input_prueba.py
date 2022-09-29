cb3aa321c4583a6e79f9440b8924cf188f1e45c0
Caro <krod2131@gmail.com>
2022-09-29 15:45:47 -0700

    probando input

---
+++ caro_exercise/input_prueba.py
@@ -0,0 +1,8 @@
+#print("Hola")
+#print("mi nombre es mamacita")
+#print("caro" + "line")
+#prueba = "bishhhhhhhh"
+#print(prueba)
+#print("caro" * 5)
+nombre = input()
+print("me caes mal " + nombre)
