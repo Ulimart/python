13a041ebbb6d0d724af2387e4020543bd08f5334
Caro <krod2131@gmail.com>
2022-10-04 10:57:09 -0700

    se hizo un cambio para el archivo len, y agrego dos nuevos archivos

--- caro_exercise/len_str_int_float.py
+++ caro_exercise/len_str_int_float.py
@@ -4,6 +4,10 @@
 #print(len("caroline"))
 #print("dime los siguientes numeros " + str(54) + str(int(60)) + str(float(21.39)))
 
-val = "caro"
-print(val)
-print(len(val))
+#val = "caro"
+#print(val)
+#print(len(val))
+
+bacon = 20
+print(bacon + 1)
+
---
+++ caro_exercise/loops_ifs.py
@@ -0,0 +1,30 @@
+#nm = "dog"
+#age = 15
+
+#if nm == "dolphin":
+#    print ("this is my animal")
+#elif nm == "tiger":
+#    print("I love its barks")
+#else:
+#    print("its name is Romina")
+
+##vamos a intentar sin definir una variable
+#if nm == "fly":
+#    print("I hate this animal")
+#elif nm == "mosquito":
+#    print("This seasos had been the worst because of the mosquitos")
+#else:
+#    print("pues no es nada")
+var = "cafe"
+var2 = "postre"
+if var == "refresco":
+    print("hace daño")
+elif var == "cafe":
+    print("vamos a tomarnos uno, yo invito")
+    if var2 == "postre":
+        print("perfecto, yo tambien quiero uno, cual se te antoja?")
+    else:
+        print("que aburrido")
+
+
+    
